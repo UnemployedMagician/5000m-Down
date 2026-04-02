d8cc629265f31a5b4af29434054b8fa1e64bd444
UnemployedMagician <breadtato@gmail.com>
2026-04-02 11:27:23 -0600

    basic environment, idle sprite

--- john_meter.gd
+++ john_meter.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 400
+# speed in pixels/sec
+var speed = 500 
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-
-func _physics_process(delta):
-	get_input()
+func _physics_process(_delta):
+# setup direction of movement
+	var direction = Input.get_vector("left", "right", "up", "down")
+# stop diagonal movement by listening for input then setting axis to zero
+	if Input.is_action_pressed("right") || Input.is_action_pressed("left"):
+		direction.y = 0
+	elif Input.is_action_pressed("up") || Input.is_action_pressed("down"):
+		direction.x = 0
+	else:
+		direction = Vector2.ZERO
+	
+#normalize the directional movement
+	direction = direction.normalized()
+# setup the actual movement
+	velocity = (direction * speed)
 	move_and_slide()
